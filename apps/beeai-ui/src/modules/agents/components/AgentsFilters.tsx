743f60c8ccd38f57021b34f65059acb6b2ee4a1b
Petr Kadlec <petr@puradesign.cz>
2025-02-10 18:26:26 +0100

    feat(ui): wip agents filter
    Signed-off-by: Petr Kadlec <petr@puradesign.cz>

--- apps/beeai-ui/src/modules/agents/components/AgentsFilters.tsx
+++ apps/beeai-ui/src/modules/agents/components/AgentsFilters.tsx
@@ -1,3 +1,23 @@
+import classes from './AgentsFilters.module.scss';
+import { IconButton, TextInput } from '@carbon/react';
+import { useId } from 'react';
+import { Filter, Search } from '@carbon/icons-react';
+
 export function AgentsFilters() {
-  return <div>TODO: AgentsFilters</div>;
+  const id = useId();
+
+  return (
+    <div className={classes.root}>
+      <div className={classes.searchBar}>
+        <Search />
+        <div className={classes.activeFilters}></div>
+        <TextInput id={`${id}:search`} labelText={undefined} />
+      </div>
+      <IconButton label="Filter" kind="tertiary" size="md" className={classes.filterBtn}>
+        <Filter />
+      </IconButton>
+
+      {/* <SearchBar search={''} onSearchChange={() => {}} /> */}
+    </div>
+  );
 }
