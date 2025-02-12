c6c699418caabcb53ffb43a20f39fcd29649905f
Petr Kadlec <petr@puradesign.cz>
2025-02-12 16:18:09 +0100

    fixup! load agent error
    Signed-off-by: Petr Kadlec <petr@puradesign.cz>

--- apps/beeai-ui/src/modules/agents/detail/AgentDetail.tsx
+++ apps/beeai-ui/src/modules/agents/detail/AgentDetail.tsx
@@ -1,9 +1,7 @@
 import { Container } from '@/components/layouts/Container';
 import { ViewStack } from '@/components/ViewStack';
 import classes from './AgentDetail.module.scss';
-import { useListAgents } from '../api/queries/useListAgents';
 import { AgentMetadata } from '../components/AgentMetadata';
-import { useMemo } from 'react';
 import { AgentTags } from '../components/AgentTags';
 import { Button, ButtonSkeleton, Layer, SkeletonPlaceholder, SkeletonText } from '@carbon/react';
 import { TextWithCopyButton } from '@/components/TextWithCopyButton/TextWithCopyButton';
@@ -11,53 +9,62 @@ import { ArrowUpRight } from '@carbon/icons-react';
 import { TagsList } from '@/components/TagsList';
 import { isStringTerminalParameterSafe } from '@/utils/strings';
 import { MarkdownContent } from '@/components/MarkdownContent/MarkdownContent';
+import { useAgent } from '../api/queries/useAgent';
+import { ErrorMessage } from '@/components/ErrorMessage/ErrorMessage';
 
 interface Props {
   name: string;
 }
 
 export function AgentDetail({ name }: Props) {
-  const { data, isPending } = useListAgents();
-
-  const agent = useMemo(() => data?.find((item) => name === item.name), [data, name]);
+  const { agent, isPending, error, refetch, isRefetching } = useAgent({ name });
 
   const runCommand = `beeai run ${isStringTerminalParameterSafe(name) ? name : `'${name}'`}`;
 
   return (
     <Container>
       <ViewStack>
-        {!isPending && agent ? (
-          <div className={classes.root}>
-            <header className={classes.header}>
-              <div className={classes.metadata}>
-                <AgentMetadata agent={agent} />
-                <AgentTags agent={agent} />
-              </div>
-              <h1>{agent?.title ?? agent.name}</h1>
-            </header>
-
-            <MarkdownContent className={classes.description}>{agent.description}</MarkdownContent>
-
-            <div className={classes.runAgent}>
-              <Layer level={1}>
-                <TextWithCopyButton text={runCommand} isCode className={classes.runCommandInput}>
-                  {runCommand}
-                </TextWithCopyButton>
-              </Layer>
-              <Button kind="primary" renderIcon={ArrowUpRight} size="md">
-                Try this agent
-              </Button>
-            </div>
-
-            {agent.fullDescription && (
-              <>
-                <hr />
-                <div className={classes.body}>
-                  <MarkdownContent>{agent.fullDescription}</MarkdownContent>
+        {!isPending ? (
+          !error && agent ? (
+            <div className={classes.root}>
+              <header className={classes.header}>
+                <div className={classes.metadata}>
+                  <AgentMetadata agent={agent} />
+                  <AgentTags agent={agent} />
                 </div>
-              </>
-            )}
-          </div>
+                <h1>{agent?.title ?? agent.name}</h1>
+              </header>
+
+              <MarkdownContent className={classes.description}>{agent.description}</MarkdownContent>
+
+              <div className={classes.runAgent}>
+                <Layer level={1}>
+                  <TextWithCopyButton text={runCommand} isCode className={classes.runCommandInput}>
+                    {runCommand}
+                  </TextWithCopyButton>
+                </Layer>
+                <Button kind="primary" renderIcon={ArrowUpRight} size="md">
+                  Try this agent
+                </Button>
+              </div>
+
+              {agent.fullDescription && (
+                <>
+                  <hr />
+                  <div className={classes.body}>
+                    <MarkdownContent>{agent.fullDescription}</MarkdownContent>
+                  </div>
+                </>
+              )}
+            </div>
+          ) : (
+            <ErrorMessage
+              title="Failed to load the agent."
+              onRetry={refetch}
+              isRefetching={isRefetching}
+              subtitle={error ? error.message : 'Agent not found.'}
+            />
+          )
         ) : (
           <AgentCardSkeleton />
         )}
