ee69f9b0568d499e177d53ae08507d2b82299a57
Lukáš Janeček <xjacka@gmail.com>
2025-02-12 10:41:46 +0100

    fixup! fixup! fixup! fixup! fixup! fixup! Merge branch 'main' into gpt_researcher_agent

--- agents/community/gpt-researcher/gpt_researcher_agent.py
+++ agents/community/gpt-researcher/gpt_researcher_agent.py
@@ -7,12 +7,19 @@ load_dotenv()
 from beeai_sdk.schemas.prompt import PromptInput, PromptOutput
 from beeai_sdk.providers.agent import run_agent_provider
     
+class Output(PromptOutput):
+    type: None | str = None
+    content: None | str = None
+    text: None | str = None
+    metadata: None | Any = None
+    
 class CustomLogsHandler:
     def __init__(self, send_progress):
         self.send_progress = send_progress
         
     async def send_json(self, data: dict[str, Any]) -> None:
-        await self.send_progress(data)
+        delta = Output(type=data.get('type'),content=data.get('content'),text=data.get('output'),metadata=data.get('metadata'))
+        await self.send_progress(delta)
 
 
 async def register_agent() -> int:
@@ -22,10 +29,10 @@ async def register_agent() -> int:
     @server.agent("GPT-researcher", 
                   "GPT Researcher is an autonomous agent designed for comprehensive web and local research on any given task.", 
                   input=PromptInput, 
-                  output=PromptOutput)
-    async def run_agent(input: PromptInput, ctx) -> PromptOutput:
+                  output=Output)
+    async def run_agent(input: PromptInput, ctx) -> Output:
 
-        async def send_progress(delta: dict[str, Any]):
+        async def send_progress(delta: Output):
             await ctx.report_agent_run_progress(delta)
             
         custom_logs_handler = CustomLogsHandler(send_progress)
@@ -35,7 +42,7 @@ async def register_agent() -> int:
         await researcher.conduct_research()
         # Write the report
         report = await researcher.write_report()
-        return PromptOutput(text=report)
+        return Output(type='result',text=report)
     
     await run_agent_provider(server)
 
