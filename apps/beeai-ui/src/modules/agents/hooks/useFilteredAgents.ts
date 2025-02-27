3b461f0b1262ff674779e755a843432311b3123c
Jan Dušek <jan@dusek.xyz>
2025-02-27 14:19:08 +0100

    fix: after merge

--- apps/beeai-ui/src/modules/agents/hooks/useFilteredAgents.ts
+++ apps/beeai-ui/src/modules/agents/hooks/useFilteredAgents.ts
@@ -16,8 +16,8 @@
 
 import { useMemo } from 'react';
 import { Agent } from '../api/types';
-import { AgentsFiltersParams } from '../types';
 import { getAgentTitle } from '../utils';
+import { AgentsFiltersParams } from '../providers/AgentsFiltersProvider';
 
 interface Props {
   agents: Agent[];
