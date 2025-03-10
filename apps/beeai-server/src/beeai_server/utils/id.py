385af411e2c3ad5dce1549806707d694110c7227
Tomas Pilar <tomas.pilar@apoco.com>
2025-03-10 16:22:22 +0100

    fixup!
    Signed-off-by: Tomas Pilar <tomas.pilar@apoco.com>

--- apps/beeai-server/src/beeai_server/utils/id.py
+++ apps/beeai-server/src/beeai_server/utils/id.py
@@ -15,14 +15,15 @@
 import os
 import platform
 import hashlib
+import binascii
 
 
-def generate_stable_id() -> str:
+def generate_stable_id(length=16) -> str:
     try:
         # Use os.uname() for Unix-like systems
         uname_info = os.uname()
         # Only use stable attributes: sysname, nodename, machine to make it more stable
-        combined_info = "".join([uname_info.sysname, uname_info.nodename, uname_info.machine]).encode("utf-8")
+        base_info = "".join([uname_info.sysname, uname_info.nodename, uname_info.machine])
     except AttributeError:
         # Fallback for Windows
         system_info = [
@@ -30,6 +31,8 @@ def generate_stable_id() -> str:
             platform.node(),  # Hostname
             platform.machine(),  # Machine type
         ]
-        combined_info = "".join(system_info).encode("utf-8")
+        base_info = "".join(system_info)
 
-    return hashlib.sha256(combined_info).hexdigest()[:16]
+    derived_key = hashlib.pbkdf2_hmac("sha256", base_info.encode("utf-8"), b"x", 1000, dklen=length)
+
+    return binascii.hexlify(derived_key).decode("utf-8")[: length * 2]  # 2 hex chars per byte
